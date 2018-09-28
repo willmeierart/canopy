ac2c994348c58bf859f6650c19d1a056a2c14253
WILL MEIER <willmeierart@gmail.com>
2018-09-28 14:53:02 -0600

    add store links

--- components/layout/StoreIcons.js
+++ components/layout/StoreIcons.js
@@ -9,9 +9,9 @@ const StoreIcons = ({ isMobile, device }) => {
     <div className='outer-container'>
       { !isMobile
         ? <div className='inner-container'>
-          <a><img alt='app store' src='/static/assets/appstore_icon.png' /></a>
-          <a><img alt='android store' src='/static/assets/google_icon.png' /></a>
-          <a><img alt='amazon store' src='/static/assets/amazon_icon.png' /></a>
+          <a href='https://itunes.apple.com/us/app/dox-a-puzzle-game/id1422501586?ls=1&mt=8'><img alt='app store' src='/static/assets/appstore_icon.png' /></a>
+          <a href='https://play.google.com/store/apps/details?id=com.canopyltd.dox'><img alt='android store' src='/static/assets/google_icon.png' /></a>
+          <a href='https://www.amazon.com/dp/B07HQ7NZXD/ref=sr_1_1?ie=UTF8&qid=1538111245&sr=8-1'><img alt='amazon store' src='/static/assets/amazon_icon.png' /></a>
         </div>
         : <div className='inner-container'>
           <a href={storeIcon().link}><img alt='store icon' src={storeIcon().img} /></a>
